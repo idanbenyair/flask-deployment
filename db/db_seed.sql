7f04be295bec082c7f9c8254e56539502632b2c8
root <root@ip-172-31-48-144.ec2.internal>
2019-04-26 22:11:57 -0400

    db changes

--- db/db_seed.sql
+++ db/db_seed.sql
@@ -1,5 +1,11 @@
 CREATE DATABASE mydb;
 USE mydb;
+
+CREATE USER 'flaskdbuser'@'%' IDENTIFIED BY 'Pa$$w0rd';
+GRANT ALL on *.* to 'flaskdbuser'@'%' ;
+
+FLUSH PRIVILEGES;
+
 CREATE TABLE mytable( 
     id INT NOT NULL AUTO_INCREMENT,
     message VARCHAR(255) NOT NULL,
